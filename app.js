43145404289260db02ccb671ec5861cb5d0267f5
Ryan Collins <ryan.k.collins222@gmail.com>
2020-04-13 14:41:49 -0700

    fixed headshot and project view bugs

---
+++ app.js
@@ -0,0 +1,29 @@
+let express = require('express');
+let data = require('./data.json');
+let {projects} = data;
+// console.log(projects);
+
+let app = express();
+
+app.set('view engine', 'pug');
+
+app.use('/static', express.static('public'));
+app.use(express.static('imgs'));
+
+// routes
+app.get('/', (req, res) => {
+  res.render('index', {projects});
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+})
+
+app.get('/project/:id', (req, res) => {
+  console.log(req.params.id);
+  res.render('project', {
+    project: projects[req.params.id - 1]
+  });
+})
+
+app.listen(3000);
